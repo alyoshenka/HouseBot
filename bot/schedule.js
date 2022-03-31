43f55bf055362bf594178831d2d6524fac64be1a
Alexi <alexibmost@gmail.com>
2022-03-31 10:34:42 -0700

    exports

---
+++ bot/schedule.js
@@ -0,0 +1,10 @@
+// get message funciton
+
+const scheduleNotifications = (notifs) => {
+  if (!notifs) {
+    console.log('No notification data provided')
+    return
+  }
+}
+
+module.exports = { scheduleNotifications }
