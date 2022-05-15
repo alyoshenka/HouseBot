808038006d3e0658bbb7eaafe03daee1db276549
Alexi <alexibmost@gmail.com>
2022-05-14 17:20:35 -0700

    play with display a bit

--- client/src/components/NotificationDisplay.js
+++ client/src/components/NotificationDisplay.js
@@ -4,18 +4,16 @@ import Typography from '@mui/material/Typography'
 import TableTest from './TableTest'
 
 const NotificationDisplay = ({ notifications, errors, loading, update }) => {
-  if (errors) {
-    return <span>Errors!</span>
-  } else if (loading) {
-    return <span>Loading...</span>
-  } else {
-    return (
-      <Box>
-        <Typography variant="h5">Saved Notifications</Typography>
-        <TableTest notifs={notifications} update={update} />
-      </Box>
-    )
-  }
+  return (
+    <Box>
+      <Typography variant="h5">Saved Notifications</Typography>
+      {errors ? (
+        <span>Errors</span>
+      ) : (
+        <TableTest notifs={loading ? null : notifications} update={update} />
+      )}
+    </Box>
+  )
 }
 
 export default NotificationDisplay
